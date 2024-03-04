ede2f4be7b2c2d594edcc0ac7e5167a94a09fc0a
ileostar <hi@leostar.top>
2024-03-04 23:10:15 +0800

    chore: home style layout

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,52 +1,101 @@
 <template>
-	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
+	<view class="homeLayout">
+		<view class="banner">
+			<swiper circular indicator-dots indicator-color="rgba(255,255,255,.5)" indicator-active-color="#fff" autoplay>
+				<swiper-item>
+					<image src="../../common/images/banner3.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../common/images/banner2.jpg" mode="aspectFill"></image>
+				</swiper-item>
+				<swiper-item>
+					<image src="../../common/images/banner1.jpg" mode="aspectFill"></image>
+				</swiper-item>
+			</swiper>
+		</view>
+		<view class="notice">
+			<view class="left">
+				<uni-icons type="sound-filled" size="20" color="#28b389"></uni-icons>
+				<text class="text">公告</text>
+			</view>
+			<view class="center">
+				<swiper vertical autoplay interval="1500" duration="300" circular>
+					<swiper-item v-for="item in 4">文字内容{{item}}</swiper-item>
+				</swiper>
+			</view>
+			<view class="right">
+				<uni-icons type="right" size="16" color="#333"></uni-icons>
+			</view>
+		</view>
+		<view class="select">
+			
 		</view>
 	</view>
 </template>
 
-<script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
-		}
-	}
+<script setup>
 </script>
 
-<style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+<style lang="scss" scoped>
+	.homeLayout {
+		.banner {
+			width: 750rpx;
+			padding: 30rpx 0;
+			swiper {
+				width: 750rpx;
+				height: 340rpx;
+				&-item {
+					width: 100%;
+					height: 100%;
+					padding: 0 30rpx;
+					image {
+						width: 100%;
+						height: 100%;
+						border-radius: 10rpx;
+					}
+				}
+			}
+		}
+		.notice {
+			display: flex;
+			align-items: center;
+			justify-content: center;
+			width: 690rpx;
+			height: 80rpx;
+			background-color: #f9f9f9;
+			margin: 0 auto;
+			border-radius: 80rpx;
+			.left {
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				width: 140rpx;
+				text {
+					padding-left: 5rpx;
+					color: #28b389;
+					font-weight: bold;
+					font-size: 30rpx;
+				}
+			}
+			.center {
+				flex: 1;
+				height: 100%;
+				swiper {
+					height: 100%;
+					&-item {
+						line-height: 80rpx;
+						height: 100%;
+						font-size: 30rpx;
+						color: #666;
+						overflow: hidden;
+						white-space: nowrap;
+						text-overflow: ellipsis;
+					}
+				}
+			}
+			.right {
+				width: 70rpx;
+			}
+		}
 	}
 </style>
